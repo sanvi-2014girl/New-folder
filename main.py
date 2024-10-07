75fc3e1a12e898218f8dbd211f0d0609fd949fff
sanvi-2014girl <goswamilipi82@gmail.com>
2024-10-07 11:27:40 +0530

    folder tk

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+#create window
+window = Tk()
+#set up 
+window.title("Codingal's class")
+window.geometry('500x500')
+window.configure(bg= "lightblue")
+#brings the output
+window.mainloop()
